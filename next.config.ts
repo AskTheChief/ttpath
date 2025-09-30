8807b027957a713b3bc1bb6e9512f00134ceb843
Ziz Seykota <zizseykota@gmail.com>
2025-09-30 00:01:52 +0000

    great, now can we make sure that our cors setting are propper

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
         source: "/api/genkit/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://ttpath.com, https://www.ttpath.com, http://ttpath.com" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
