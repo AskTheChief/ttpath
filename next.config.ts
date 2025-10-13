fd7e3b8be37dd72299b1f7154aa03012b2e54745
Ziz Seykota <zizseykota@gmail.com>
2025-10-13 18:09:53 +0000

    i want all traffic to redirect to ttpath.net

--- next.config.ts
+++ next.config.ts
@@ -38,7 +38,7 @@ const nextConfig: NextConfig = {
         source: "/api/genkit/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "https://ttpath.com, https://www.ttpath.com, http://ttpath.com" },
+          { key: "Access-Control-Allow-Origin", value: "https://ttpath.net, https://www.ttpath.net, http://ttpath.net" },
           { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
@@ -52,10 +52,10 @@ const nextConfig: NextConfig = {
         has: [
           {
             type: 'host',
-            value: 'ttpath.com',
+            value: 'www.ttpath.net',
           },
         ],
-        destination: 'https://www.ttpath.com/:path*',
+        destination: 'https://ttpath.net/:path*',
         permanent: true,
       },
     ]
