93e14ef10ea9baa2db2265ccb3b33e323cd63f23
Ziz Seykota <zizseykota@gmail.com>
2026-02-04 22:28:50 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="antialiased">
+    <html lang="en" className="antialiased" suppressHydrationWarning>
       <body className={`${inter.variable} ${kalam.variable} font-sans`}>
         {children}
         <Toaster />
